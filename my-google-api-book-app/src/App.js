df141eb151735d48a1bebe4097e4fd9a6dcc8a7a
JizongL <2015rpro@gmail.com>
2019-03-24 21:29:34 -0400

    testing google api

--- my-google-api-book-app/src/App.js
+++ my-google-api-book-app/src/App.js
@@ -5,7 +5,43 @@ import BookList from './booksList/bookList'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      bookList:[],
+      searchTerm:'',
+      printType:'',
+      bookType:'',
+      error:''
+    }
+  }
+
+
+
+  componentDidMount(){
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=time:keyes&key=AIzaSyAUoZApaq9eRmbV9OaTTHFzrLSoeXCDbsA&printType=magazines'
+    
+    fetch(url)
+    .then(res=>{if(!res.ok){
+      throw new Error(res.statusText)
+    }
+    return res.json()
+  })
+  .then(
+    data =>{
+      this.setState({
+        bookmarks:data,
+        error:null
+      })
+    }
+  ).catch(err=>this.setState({
+    error:err.message
+  }))
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <BookToolBar />
